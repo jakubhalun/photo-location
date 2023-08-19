4a32f8b5012ce65fddf8015750538f8eb50fee0a
Jakub Hałun <jakubhal@gmail.com>
2023-08-19 13:55:06 +0200

    time adjustments

--- app/src/main/kotlin/pl/halun/tools/photo/location/MainWindowController.kt
+++ app/src/main/kotlin/pl/halun/tools/photo/location/MainWindowController.kt
@@ -1,6 +1,9 @@
 package pl.halun.tools.photo.location
 
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
 import javafx.fxml.FXML
+import javafx.scene.control.ComboBox
 import javafx.scene.control.TextArea
 import javafx.scene.input.DragEvent
 import javafx.scene.input.Dragboard
@@ -9,6 +12,7 @@ import pl.halun.tools.photo.location.jpegs.InvalidJpegInputFileException
 import pl.halun.tools.photo.location.jpegs.JpegReader
 import pl.halun.tools.photo.location.kmls.KmlReader
 import pl.halun.tools.photo.location.main.LocationInTimeTextProvider
+import java.time.Duration
 import java.time.Instant
 
 class MainWindowController {
@@ -30,6 +34,18 @@ class MainWindowController {
     lateinit var outputTextArea: TextArea
 
     @FXML
+    lateinit var timeZoneOffsetComboBox: ComboBox<String>
+
+    val timeZoneOffsets: ObservableList<String> = FXCollections.observableArrayList<String>().apply {
+        for (i in -12..14) {
+            add(String.format("%+d:00", i))
+            if (i != 12 && i != 13) {
+                add(String.format("%+d:30", i))
+            }
+        }
+    }
+
+    @FXML
     fun onJpegDragOver(dragEvent: DragEvent) {
         if (dragEvent.dragboard.hasFiles()) {
             dragEvent.acceptTransferModes(TransferMode.COPY)
@@ -49,19 +65,29 @@ class MainWindowController {
     fun onJpegDrop(dragEvent: DragEvent) {
         val db: Dragboard = dragEvent.dragboard
         if (db.hasFiles()) {
-            loadJpegData(db.files[0].absolutePath)
+            val durationOffset = adjustTimeWithOffset()
+            loadJpegData(db.files[0].absolutePath, durationOffset)
         }
         dragEvent.isDropCompleted = true
         dragEvent.consume()
     }
 
-    private fun loadJpegData(path: String) =
+    private fun adjustTimeWithOffset(): Duration {
+        val offsetString = timeZoneOffsetComboBox.value
+        val hours = offsetString.substring(0, 3).toInt()
+        val minutes = offsetString.substring(4).toInt()
+
+        return Duration.ofHours(hours.toLong()).plus(Duration.ofMinutes(minutes.toLong()))
+    }
+
+    private fun loadJpegData(path: String, durationOffset: Duration) {
         try {
-            val creationTime = jpegReader.readCreationTime(path)
+            val creationTime = jpegReader.readCreationTime(path, durationOffset)
             updateOutput(creationTime)
         } catch (e: InvalidJpegInputFileException) {
             jpegInputArea.text = e.message
         }
+    }
 
     @FXML
     fun onKmlDrop(dragEvent: DragEvent) {
--- app/src/main/kotlin/pl/halun/tools/photo/location/jpegs/JpegReader.kt
+++ app/src/main/kotlin/pl/halun/tools/photo/location/jpegs/JpegReader.kt
@@ -4,14 +4,15 @@ import com.drew.imaging.ImageMetadataReader
 import com.drew.metadata.Metadata
 import com.drew.metadata.exif.ExifIFD0Directory
 import java.io.File
+import java.time.Duration
 import java.time.Instant
 
 class JpegReader {
-    fun readCreationTime(path: String): Instant {
-        return getOriginalCreationDate(path)
+    fun readCreationTime(path: String, durationOffset: Duration): Instant {
+        return getOriginalCreationDate(path, durationOffset)
     }
 
-    private fun getOriginalCreationDate(path: String): Instant {
+    private fun getOriginalCreationDate(path: String, durationOffset: Duration): Instant {
         val file = File(path)
         if (!file.exists()) throw InvalidJpegInputFileException("File does not exist!")
         if (file.isDirectory) throw InvalidJpegInputFileException("Single JPEG file is required, not a directory")
@@ -27,8 +28,10 @@ class JpegReader {
         val directory = metadata.getFirstDirectoryOfType(ExifIFD0Directory::class.java)
             ?: throw InvalidJpegInputFileException("This file does not contain valid EXIF")
 
-        return directory.getDate(ExifIFD0Directory.TAG_DATETIME)
+        val unadjustedInstant = directory.getDate(ExifIFD0Directory.TAG_DATETIME)
             ?.toInstant() ?: throw InvalidJpegInputFileException("Cannot find creation date")
+
+        return unadjustedInstant.plus(durationOffset)
     }
 }
 
